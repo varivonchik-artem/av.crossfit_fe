c0f8f9d3675857f493a7e7889034df131505c912
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-22 23:16:01 +0300

    chore: establish initial project foundation

---
+++ configuration/types/configurationTypes.ts
@@ -0,0 +1,19 @@
+export type BuildMode = "development" | "production";
+
+export interface BuildPaths {
+  entry: string;
+  build: string;
+  html: string;
+}
+
+export interface BuildEnvironmentVariables {
+  mode: BuildMode;
+  port: number;
+}
+
+export interface BuildOptions {
+  mode: BuildMode;
+  paths: BuildPaths;
+  port: number;
+  isDevelopment: boolean;
+}
---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import webpack from "webpack";
+import { buildWebpackConfiguration } from "./configuration/build/buildWebpackConfiguration";
+import { BuildEnvironmentVariables, BuildPaths } from "./configuration/types/configurationTypes";
+
+export default (env: BuildEnvironmentVariables) => {
+  const PATHS: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const MODE = env.mode || "development";
+  const IS_DEVELOPMENT = MODE === "development";
+
+  const PORT = env.port || 3000;
+
+  const webpackConfiguration: webpack.Configuration = buildWebpackConfiguration({
+    mode: MODE,
+    paths: PATHS,
+    port: PORT,
+    isDevelopment: IS_DEVELOPMENT,
+  });
+
+  return webpackConfiguration;
+};
